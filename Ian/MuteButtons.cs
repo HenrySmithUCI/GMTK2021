79ead302d79a40a9ebb0aa064383845542f30836
IanHurd <Iarbobray@gmail.com>
2021-06-12 17:43:57 -0700

    Mute Buttons

--- Ian/MuteButtons.cs
+++ Ian/MuteButtons.cs
@@ -3,9 +3,16 @@ using System;
 
 public class MuteButtons : HBoxContainer
 {
+    public override void _Ready()
+    {
+        GetNode<CheckBox>("MusicMute").Pressed = AudioServer.IsBusMute(2);
+        GetNode<CheckBox>("EffectsMute").Pressed = AudioServer.IsBusMute(1);
+
+    }
+
     public void toggleMusicMute(bool val)
     {
-        AudioServer.SetBusMute(0, val);
+        AudioServer.SetBusMute(2, val);
     }
 
     public void toggleEffectsMute(bool val)
